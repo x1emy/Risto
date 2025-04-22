493f4a7049034d40c6f92377c40fa3b6d22df09c
ballgimn <meytopintheworld@gmail.com>
2025-04-23 00:24:44 +0500

    finished restaraunts pages

--- booking-frontend/src/app/refugio/refugio.component.ts
+++ booking-frontend/src/app/refugio/refugio.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-refugio',
-  imports: [],
+  standalone:true,
+  imports: [ CommonModule],
   templateUrl: './refugio.component.html',
   styleUrl: './refugio.component.css'
 })
 export class RefugioComponent {
+  slides = [
+    {img: "/assets/rifugio/3.jpg"},
+    {img: "/assets/rifugio/2.jpg"},
+    {img: "/assets/rifugio/1.jpg"},
+    {img: "/assets/rifugio/2.jpg"}
+  ];
 
 }
